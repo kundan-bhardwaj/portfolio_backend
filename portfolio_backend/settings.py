39774e977be25a59d2802353d25f462ad69d89d9
kundan <kundanbhardwaj145@gmail.com>
2024-06-04 09:43:45 +0530

    cap

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
